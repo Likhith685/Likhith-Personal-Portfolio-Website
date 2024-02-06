0acd3ce91176d83ef5e88bb7b6ef8c99f4f3d58a
Likhith685 <likhith685@gmail.com>
2024-02-06 22:48:47 +0530

    update1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function () {
     // Get reference to the button
     const goToNextPageBtn = document.getElementById('goToNextPage');
@@ -9,15 +8,3 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = 'index.html';
     });
 });
-=======
-document.addEventListener('DOMContentLoaded', function () {
-    // Get reference to the button
-    const goToNextPageBtn = document.getElementById('goToNextPage');
-
-    // Event listener for button click
-    goToNextPageBtn.addEventListener('click', function () {
-        // Navigate to the next page
-        window.location.href = 'index.html';
-    });
-});
->>>>>>> e3056f908f5a40065c3473a7c7f3899e747d67e4
