a7a237fa8d45a98bb78315e4af12de8d9fb2f9f7
Likhith685 <142077142+Likhith685@users.noreply.github.com>
2024-02-06 22:38:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get reference to the button
+    const goToNextPageBtn = document.getElementById('goToNextPage');
+
+    // Event listener for button click
+    goToNextPageBtn.addEventListener('click', function () {
+        // Navigate to the next page
+        window.location.href = 'index.html';
+    });
+});
